7f2a31b8a2c32077c97b48f57427bdccfa83dc0d
Tejaswi Digumarti <tejaswi.digumarti@disneyresearch.com>
2019-03-24 22:00:21 +0100

    Added 10x8 layout for keys

--- keyboards/ocopus/config.h
+++ keyboards/ocopus/config.h
@@ -28,8 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DESCRIPTION     A custom keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 9
-#define MATRIX_COLS 9
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
 
 /*
  * Keyboard Matrix Assignments
--- keyboards/ocopus/ocopus.h
+++ keyboards/ocopus/ocopus.h
@@ -1,4 +1,4 @@
-/* Copyright 2019 %YOUR_NAME%
+/* Copyright 2019 Sundara Tejaswi Digumarti
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -26,10 +26,23 @@
  * represents the switch matrix.
  */
 #define LAYOUT( \
-    k00, k01, k02, \
-      k10,  k11   \
+  			k03, k04, k05, k06, k07,      				 k08, k09, k10, k11, k12, \
+  k00, k01, k02, k20, k21, k22, k23,           			 k24, k25, k26, k27, k13, k14, K15, \
+  k16, k17, k18, k19, k37, k38, k39,                	 k40, k41, k42, k28, k29, k30, k31, \
+  k32, k33, k34, k35, k36, k53, k54,       				 k57, k58, k43, k44, k45, k46, k47, \
+  k48, k49, k50, k51, k52, 		k55,	      			 k56,      k59, k60, k61, k62, K63, \
+  k64, k65, k66, k67, 				k68, k69	k74, k75, 		        k76, k77, k78, k79, \
+  									k71, k70 	k73, k72 \
 ) \
 { \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k11 }, \
+    { k00, k01, k02, k03, k04, k05, k06, k07 }, \
+    { k16, k17, k18, k19, k20, k21, k22, k23 }, \
+    { k32, k33, k34, k35, k36, k37, k38, k39 }, \
+    { k48, k49, k50, k51, k52, k53, k54, k55 }, \
+    { k64, k65, k66, k67, k68, k69, k70, k71 }, \
+    { k15, k14, k13, k12, k11, k10, k09, k08 }, \
+    { k31, k30, k29, k28, k27, k26, k25, k24 }, \
+    { k47, k46, k45, k44, k43, k42, k41, k40 }, \
+    { k63, k62, k61, k60, k59, k58, k57, k56 }, \
+    { k79, k78, k77, k76, k75, k74, k73, k72 }, \
 }
