4211a970f124ffa4a2dbb5f3c215a6dbe36817b2
Tejaswi Digumarti <tejaswi.digumarti@disneyresearch.com>
2019-04-13 13:10:38 +0200

    Updated config file with layout and added arduino pins

--- keyboards/handwired/ocopus/config.h
+++ keyboards/handwired/ocopus/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 %YOUR_NAME%
+Copyright 2019 %Sundara Tejaswi Digumarti%
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -25,11 +25,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Sundara Tejaswi Digumarti
 #define PRODUCT         ocopus keyboard
-#define DESCRIPTION     A custom keyboard
+#define DESCRIPTION     A custom split keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 10
-#define MATRIX_COLS 8
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 9
 
 /*
  * Keyboard Matrix Assignments
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
 */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { D4, D7, E6, B4 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6, B5 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
